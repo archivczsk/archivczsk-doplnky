61b6b617adac094864fb0310d11c4bb05cb938b1
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2022-05-06 13:54:27 +0200

    fixed device id creation on o2tv

--- plugin.video.archivo2tv/default.py
+++ plugin.video.archivo2tv/default.py
@@ -21,6 +21,12 @@ __language__ = __addon__.getLocalizedString
 
 settings = {'quality':__addon__.getSetting('quality')}
 
-provider = O2tvContentProvider( username=__addon__.getSetting('username'), password=__addon__.getSetting('password'), device_id=__addon__.getSetting( 'deviceid' ), data_dir=__addon__.getAddonInfo('profile'), session=session )
+device_id = __addon__.getSetting( 'deviceid' )
+if not device_id or len(device_id) == 0:
+	import random, string
+	device_id = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(15))
+	__addon__.setSetting("deviceid", device_id)
+
+provider = O2tvContentProvider( username=__addon__.getSetting('username'), password=__addon__.getSetting('password'), device_id=device_id, data_dir=__addon__.getAddonInfo('profile'), session=session )
 
 xbmcprovider.XBMCMultiResolverContentProvider(provider, settings, __addon__, session).run(params)
