0c138c7ce096b9cbf6b718b747af819493b57206
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2022-06-21 15:14:02 +0200

    rewrite orangetv addon to use services and autostart

---
+++ plugin.video.orangetv/orangetv_autostart.py
@@ -0,0 +1,57 @@
+try:
+	sys.path.append( os.path.dirname(__file__) )
+except:
+	pass
+
+import traceback
+import base64
+from Plugins.Extensions.archivCZSK.version import version
+from Plugins.Extensions.archivCZSK.engine.client import log
+from Plugins.Extensions.archivCZSK.engine.httpserver import archivCZSKHttpServer, AddonHttpRequestHandler
+
+from Plugins.Extensions.archivCZSK.archivczsk import ArchivCZSK
+from .orangetv import OrangeTVcache
+
+# #################################################################################################
+
+__scriptid__ = 'plugin.video.orangetv'
+__addon__ = ArchivCZSK.get_xbmc_addon(__scriptid__)
+__language__ = __addon__.getLocalizedString
+
+
+class OrangetvHTTPRequestHandler( AddonHttpRequestHandler ):
+	def __init__(self):
+		AddonHttpRequestHandler.__init__(self, __scriptid__)
+
+	def P_playlive(self, request, path):
+		try:
+			username=__addon__.getSetting('orangetvuser')
+			password=__addon__.getSetting('orangetvpwd')
+			device_id=__addon__.getSetting( 'deviceid' )
+			data_dir=__addon__.getAddonInfo('profile')
+
+			orangetv = OrangeTVcache.get(username, password, device_id, data_dir, log.info )
+			path = base64.b64decode(path).decode("utf-8")
+			result = orangetv.getVideoLink(path + '|||')
+			location = result[0]['url']
+			log.debug("Resolved stream address: %s" % location )
+		except:
+			log.error(traceback.format_exc())
+			return self.reply_error500( request )
+
+		return self.reply_redirect( request, location)
+	
+	def default_handler(self, request, path_full ):
+		data = "Default handler pre orange pre path: %s" % path_full
+		return self.reply_ok( request, data.encode('utf-8'), "text/plain; charset=UTF-8")
+
+		
+#		request.write( data.encode('utf-8') )
+#		request.finish()
+#		return server.NOT_DONE_YET
+#		return data.encode('utf-8')
+
+request_handler = OrangetvHTTPRequestHandler()
+
+archivCZSKHttpServer.registerRequestHandler( request_handler )
+log.info( "OrangeTV http endpoint: %s" % archivCZSKHttpServer.getAddonEndpoint( request_handler ) )
--- plugin.video.orangetv/orangetv_provider.py
+++ plugin.video.orangetv/orangetv_provider.py
@@ -22,7 +22,7 @@ from Plugins.Extensions.archivCZSK.engine import client
 
 from Plugins.Extensions.archivCZSK.archivczsk import ArchivCZSK
 
-from orangetv import OrangeTV
+from orangetv import OrangeTVcache
 from lameDB import lameDB
 import util
 from provider import ContentProvider
@@ -37,10 +37,6 @@ except:
 
 __addon__ = ArchivCZSK.get_xbmc_addon('plugin.video.orangetv')
 
-proxy_url = "http://127.0.0.1:18081"
-PROXY_VER='1'
-
-
 def _to_string(text):
 	if type(text).__name__ == 'unicode':
 		output = text.encode('utf-8')
@@ -49,215 +45,11 @@ def _to_string(text):
 	return output
 
 
-# bouquet generator funcions based on AntikTV addon
-
-def download_picons( picons ):
-	if not os.path.exists( '/usr/share/enigma2/picon' ):
-		os.mkdir('/usr/share/enigma2/picon')
-		
-	for ref in picons:
-		if not picons[ref].endswith('.png'):
-			continue
-		
-		fileout = '/usr/share/enigma2/picon/' + ref + '.png'
-		
-		if not os.path.exists(fileout):
-			try:
-				r = requests.get( picons[ref].replace('https://', 'http://').replace('64x64','220x220'), timeout=3 )
-				if r.status_code == 200:
-					with open(fileout, 'wb') as f:
-						f.write( r.content )
-			except:
-				pass
-
-def build_service_ref( service, player_id ):
-	return player_id + ":0:{:X}:{:X}:{:X}:{:X}:{:X}:0:0:0:".format( service.ServiceType, service.ServiceID, service.Transponder.TransportStreamID, service.Transponder.OriginalNetworkID, service.Transponder.DVBNameSpace )
-				
-def service_ref_get( lamedb, channel_name, player_id, channel_id ):
-	
-	skylink_freq = [ 11739, 11778, 11856, 11876, 11934, 11954, 11973, 12012, 12032, 12070, 12090, 12110, 12129, 12168, 12344, 12363 ]
-	antik_freq = [ 11055, 11094, 11231, 11283, 11324, 11471, 11554, 11595, 11637, 12605 ]
-	
-	def cmp_freq( f, f_list ):
-		f = int(f/1000)
-		
-		for f2 in f_list:
-			if abs( f - f2) < 5:
-				return True
-	
-		return False
-
-	if lamedb != None:
-		try:
-			services = lamedb.Services[ lamedb.name_normalise( channel_name ) ]
-			
-			# try position 23.5E first
-			for s in services:
-				if s.Transponder.Data.OrbitalPosition == 235 and cmp_freq( s.Transponder.Data.Frequency, skylink_freq ):
-					return build_service_ref(s, player_id)
-	
-			# then 16E
-			for s in services:
-				if s.Transponder.Data.OrbitalPosition == 160 and cmp_freq( s.Transponder.Data.Frequency, antik_freq ):
-					return build_service_ref(s, player_id)
-	
-			for s in services:
-				if s.Transponder.Data.OrbitalPosition == 235:
-					return build_service_ref(s, player_id)
-	
-			# then 16E
-			for s in services:
-				if s.Transponder.Data.OrbitalPosition == 160:
-					return build_service_ref(s, player_id)
-	
-			# then 0,8W
-			for s in services:
-				if s.Transponder.Data.OrbitalPosition == -8:
-					return build_service_ref(s, player_id)
-	
-			# then 192
-			for s in services:
-				if s.Transponder.Data.OrbitalPosition == 192:
-					return build_service_ref(s, player_id)
-	
-			# take the first one
-			for s in services:
-				return build_service_ref(s, player_id)
-	
-		except:
-			pass
-	
-	return player_id + ":0:1:%X:0:0:E020000:0:0:0:" % channel_id
-
-
-def generate_bouquet(channels, enable_adult=True):
-	# if epg generator is disabled, then try to create service references based on lamedb
-	if __addon__.getSetting('enable_xmlepg').lower() == 'true':
-		lamedb = None
-	else:
-		lamedb = lameDB("/etc/enigma2/lamedb")
-	
-	player_name = __addon__.getSetting('player_name')
-	
-	if player_name == "1": # gstplayer
-		player_id = "5001"
-	elif player_name == "2": # exteplayer3
-		player_id = "5002"
-	elif player_name == "3": # DMM
-		player_id = "8193"
-	elif player_name == "4": # DVB service (OE >=2.5)
-		player_id = "1"
-	else:
-		player_id = "4097" # system default
-
-	file_name = "userbouquet.orangetv.tv"
-	
-	picons = {}
-	picons_enabled = __addon__.getSetting('enable_picons').lower() == 'true'
-	
-	with open( "/etc/enigma2/" + file_name, "w" ) as f:
-		f.write( "#NAME Orange TV\n")
-		
-		for channel in channels:
-			if not enable_adult and channel.adult:
-				continue
-			
-			channel_name = _to_string( channel.name )
-			url = proxy_url + '/playlive/' + _to_string(channel.channel_key)
-			url = quote( url )
-			
-			service_ref = service_ref_get( lamedb, channel_name, player_id, channel.id )
-				
-			f.write( "#SERVICE " + service_ref + url + ":" + channel_name + "\n")
-			f.write( "#DESCRIPTION " + channel_name + "\n")
-			
-			try:
-				if picons_enabled and service_ref.endswith(':E020000:0:0:0:'):
-					picons[ service_ref[:-1].replace(':', '_') ] = channel.picon
-			except:
-				pass
-	
-	first_export = True
-	with open( "/etc/enigma2/bouquets.tv", "r" ) as f:
-		for line in f.readlines():
-			if file_name in line:
-				first_export = False
-				break
-	
-	if first_export:
-		with open( "/etc/enigma2/bouquets.tv", "a" ) as f:
-			f.write( '#SERVICE 1:7:1:0:0:0:0:0:0:0:FROM BOUQUET "' + file_name + '" ORDER BY bouquet' + "\n" )
-
-	try:
-		requests.get("http://127.0.0.1/web/servicelistreload?mode=2")
-	except:
-		pass
-	
-	if picons_enabled:
-		threading.Thread(target=download_picons,args=(picons,)).start()
-	
-	
-	return "OrangeTV userbouquet vygenerovaný"
-
-def install_orangetv_proxy():
-	src_file = os.path.dirname(__file__) + '/orangetv_proxy.sh'
-	
-	for i in range(3):
-		try:
-			response = requests.get( proxy_url + '/info', timeout=2 )
-			
-			if response.text.startswith("orangetv_proxy"):
-				if response.text == "orangetv_proxy" + PROXY_VER:
-					# current running version match
-					return True
-				else:
-					os.chmod( src_file, 0o755 )
-					# wrong runnig version - restart it and try again
-					os.system('/etc/init.d/orangetv_proxy.sh stop')
-					time.sleep(2)
-					os.system('/etc/init.d/orangetv_proxy.sh start')
-					time.sleep(2)
-			else:
-				# incorrect response - install new orangetv proxy
-				break
-		except:
-			# something's wrong - we will try again
-			pass
-	
-	os.chmod( src_file, 0o755 )
-	try:
-		os.symlink( src_file, '/etc/init.d/orangetv_proxy.sh' )
-	except:
-		pass
-	
-	try:
-		os.system('update-rc.d orangetv_proxy.sh defaults')
-		os.system('/etc/init.d/orangetv_proxy.sh start')
-		time.sleep(1)
-	except:
-		pass
-
-	for i in range(5):
-		try:
-			response = requests.get( proxy_url + '/info', timeout=1 )
-		except:
-			response = None
-			time.sleep(1)
-			pass
-		
-		if response != None and response.text == "orangetv_proxy" + PROXY_VER:
-			return True		
-	
-	return False
-
-
 day_translation = {"Monday": "Pondelok", "Tuesday": "Utorok", "Wednesday": "Streda", "Thursday": "Štvrtok", "Friday": "Piatok", "Saturday": "Sobota", "Sunday": "Nedeľa"}
 day_translation_short = {"Monday": "Po", "Tuesday": "Ut", "Wednesday": "St", "Thursday": "Št", "Friday": "Pi", "Saturday": "So", "Sunday": "Ne"}
 
 
 class orangetvContentProvider(ContentProvider):
-	orangetv = None
-	orangetv_init_params = None
 
 	
 	# #################################################################################################
@@ -265,20 +57,8 @@ class orangetvContentProvider(ContentProvider):
 	def __init__(self, username=None, password=None, device_id=None, data_dir=None, session=None, filter=None, tmp_dir='/tmp'):
 		ContentProvider.__init__(self, 'orangetv', '/', username, password, filter, tmp_dir)
 		
-		self.username = username
-		self.password = password
-		self.device_id = device_id
 		self.session = session
-		
-		if orangetvContentProvider.orangetv and orangetvContentProvider.orangetv_init_params == (username, password, device_id):
-			self.info("OrangeTV already loaded")
-		else:
-			orangetvContentProvider.orangetv = OrangeTV(username, password, device_id, data_dir, self.info )
-			orangetvContentProvider.orangetv_init_params = (username, password, device_id)
-			orangetvContentProvider.orangetv.loadEpgCache()
-			self.info("New instance of OrangeTV initialised")
-		
-		self.orangetv = orangetvContentProvider.orangetv
+		self.orangetv = OrangeTVcache.get(username, password, device_id, data_dir, client.log.info )
 
 	# #################################################################################################
 
@@ -345,13 +125,8 @@ class orangetvContentProvider(ContentProvider):
 				result.append(item)
 				
 		elif section == '#bouquet_tv':
-			msg = generate_bouquet(self.orangetv.live_channels(), __addon__.getSetting('enable_adult').lower() == 'true')
-			msg = ""
-			if install_orangetv_proxy():
-				msg += "\nOrangeTV proxy funguje."
-			else:
-				msg += "\nZlyhalo spustenie OrangeTV proxy servera"
-			return msg
+			client.add_operation('SEND_SERVICE_COMMAND', { 'cmd': "userbouquet_gen", 'msg': "Generovanie userbouquetu bolo spustené", 'msgType': 'info', 'msgTimeout': 3, 'canClose': True })
+			return None
 		elif section.startswith('#deldev#'):
 			dev_name = section[8:]
 			self.orangetv.device_remove(dev_name)
