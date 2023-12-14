645b2663c67528f3b00fd46aedb3374560d45bff
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2023-12-14 16:36:20 +0100

    improvements in HLS handler

--- tools_archivczsk/http_handler/hls.py
+++ tools_archivczsk/http_handler/hls.py
@@ -7,6 +7,17 @@ import requests
 import re
 import json
 
+from twisted.web.client import CookieAgent, RedirectAgent, Agent, HTTPConnectionPool
+from twisted.web.http_headers import Headers
+from twisted.internet.protocol import Protocol
+from twisted.internet import reactor
+
+try:
+	from cookielib import CookieJar
+except:
+	from http.cookiejar import CookieJar
+
+
 # #################################################################################################
 
 def stream_key_to_hls_url(endpoint, stream_key):
@@ -25,18 +36,52 @@ def stream_key_to_hls_url(endpoint, stream_key):
 
 # #################################################################################################
 
+class HlsSegmentDataWriter(Protocol):
+	def __init__(self, cp, d, cbk_data, timeout_call ):
+		self.cp = cp
+		self.d = d # reference to defered object - needed to ensure, that it is not destroyed during operation
+		self.cbk_data = cbk_data
+		self.timeout_call = timeout_call
+
+	def dataReceived(self, data):
+		try:
+			self.cbk_data(data)
+		except:
+			self.cp.log_exception()
+
+	def connectionLost(self, reason):
+		try:
+			self.cbk_data(None)
+		except:
+			self.cp.log_exception()
+
+		if self.timeout_call.active():
+			self.timeout_call.cancel()
+
+# #################################################################################################
 
 class HlsHTTPRequestHandler(AddonHttpRequestHandler):
 	'''
 	Http request handler that implements processing of HLS master playlist and exports new one with only selected one video stream.
 	Other streams (audio, subtitles, ...) are preserved.
 	'''
-	def __init__(self, content_provider, addon):
+	def __init__(self, content_provider, addon, proxy_segments=False):
 		AddonHttpRequestHandler.__init__(self, addon)
 		self.cp = content_provider
-		self.hls_session = requests.Session()
+		self.proxy_segments = proxy_segments
+
+		timeout = int(self.cp.get_setting('loading_timeout'))
+		if timeout == 0:
+			timeout = 5 # it will be very silly to disable timeout, so set 5s here as default
+
+		self._cookies = CookieJar()
+		self._pool = HTTPConnectionPool(reactor)
+		self.cookie_agent = Agent(reactor, connectTimeout=timeout/2, pool=self._pool)
+		self.cookie_agent = CookieAgent(self.cookie_agent, self._cookies)
+		self.cookie_agent = RedirectAgent(self.cookie_agent)
 
 	# #################################################################################################
+
 	def decode_stream_key(self, path):
 		'''
 		Decodes stream key encoded using stream_key_to_hls_url
@@ -69,6 +114,17 @@ class HlsHTTPRequestHandler(AddonHttpRequestHandler):
 		'''
 		Handles request to hls master playlist. Address is created using stream_key_to_hls_url()
 		'''
+
+		def hls_continue(data):
+			if data:
+				request.setResponseCode(200)
+				request.setHeader('content-type', "application/vnd.apple.mpegurl")
+				request.write(data.encode('utf-8'))
+			else:
+				request.setResponseCode(401)
+			request.finish()
+
+
 		try:
 			stream_key = self.decode_stream_key(path)
 
@@ -81,94 +137,276 @@ class HlsHTTPRequestHandler(AddonHttpRequestHandler):
 				hls_info = { 'url': hls_info }
 
 			# resolve HLS playlist and get only best stream
-			data = self.get_hls_playlist_data(hls_info['url'], hls_info.get('bandwidth'), hls_info.get('headers'))
-
-			if not data:
-				self.reply_error404(request)
+			self.get_hls_playlist_data_async(hls_info['url'], hls_info.get('bandwidth'), hls_info.get('headers'), cbk=hls_continue)
+			return self.NOT_DONE_YET
 
 		except:
 			self.cp.log_exception()
 			return self.reply_error500(request)
 
-		return self.reply_ok(request, data, "application/vnd.apple.mpegurl")
 
 	# #################################################################################################
 
-	def get_hls_playlist_data(self, url, bandwidth=None, headers=None):
+	def proxify_segment_url(self, url):
+		'''
+		Redirects segment url to use proxy
+		'''
+
+		segment_key = base64.b64encode(url.encode('utf-8')).decode('utf-8')
+		return "/%s/s/%s" % (self.name, segment_key)
+
+	# #################################################################################################
+
+	def proxify_playlist_url(self, url):
+		'''
+		Redirects segment url to use proxy
+		'''
+
+		segment_key = base64.b64encode(url.encode('utf-8')).decode('utf-8')
+		return "/%s/p/%s" % (self.name, segment_key)
+
+	# #################################################################################################
+
+	def P_s(self, request, path):
+		url = base64.b64decode(path.encode('utf-8')).decode('utf-8')
+		
+		self.cp.log_debug('Requesting HLS segment: %s' % url)
+		flags = {
+			'finished': False
+		}
+		def http_code_write( code, rurl ):
+			request.setResponseCode(code)
+
+		def http_header_write( k, v ):
+			request.setHeader(k, v)
+
+		def http_data_write( data ):
+			if data != None:
+				request.write(data)
+			else:
+				if flags['finished'] == False:
+					request.finish()
+
+		def request_finished(reason):
+			flags['finished'] = True
+
+		self.request_http_data_async(url, http_code_write, http_header_write, http_data_write, range=request.getHeader(b'Range'))
+		request.notifyFinish().addBoth(request_finished)
+		return self.NOT_DONE_YET
+	
+	# #################################################################################################
+
+	def request_http_data_async(self, url, cbk_response_code, cbk_header, cbk_data, headers=None, range=None):
+		timeout = int(self.cp.get_setting('loading_timeout'))
+		if timeout == 0:
+			timeout = 5 # it will be very silly to disable timeout, so set 5s here as default
+
+		request_headers = Headers()
+
+		if headers:
+			for k, v in headers.items():
+				request_headers.addRawHeader(k.encode('utf-8'), v.encode('utf-8'))
+
+		if range != None:
+			request_headers.addRawHeader(b'Range', range)
+
+		if not request_headers.hasHeader('User-Agent'):
+			# empty user agent is not what we want, so use something common
+			request_headers.addRawHeader(b'User-Agent', b'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
+
+		d = self.cookie_agent.request( b'GET', url.encode('utf-8'), request_headers, None)
+
+		timeout_call = reactor.callLater(timeout, d.cancel)
+
+		def request_created(response):
+			cbk_response_code(response.code, response.request.absoluteURI)
+
+			for k,v in response.headers.getAllRawHeaders():
+				if k in (b'Content-Length', b'Accept-Ranges', b'Content-Type', b'Accept'):
+					cbk_header(k,v[0])
+
+			response.deliverBody(HlsSegmentDataWriter(self.cp, d, cbk_data, timeout_call))
+
+		def request_failed(response):
+			self.cp.log_error('Request for url %s failed: %s' % (url, str(response)))
+
+			cbk_response_code(500, '')
+			cbk_data(None)
+			if timeout_call.active():
+				timeout_call.cancel()
+
+		d.addCallback(request_created)
+		d.addErrback(request_failed)
+
+	# #################################################################################################
+
+	def request_http_data_async_simple(self, url, cbk, headers=None, **kwargs):
+		response = {
+			'status_code': None,
+			'url': None,
+			'headers': {},
+			'content': b''
+		}
+
+		def cbk_response_code(rcode, rurl):
+			response['status_code'] = rcode
+			response['url'] = rurl.decode('utf-8')
+
+		def cbk_header(k, v):
+			response['headers'][k] = v
+
+		def cbk_data(data):
+			if data == None:
+				try:
+					cbk(response, **kwargs)
+				except:
+					self.cp.log_exception()
+			else:
+				response['content'] += data
+		
+		return self.request_http_data_async( url, cbk_response_code, cbk_header, cbk_data, headers=headers)
+		
+	# #################################################################################################
+
+	def P_p(self, request, path):
+		url = base64.b64decode(path.encode('utf-8')).decode('utf-8')
+		
+		self.cp.log_debug('Requesting HLS variant playlist: %s' % url)
+
+		def p_continue(response):
+#			self.cp.log_debug('Received HLS variant response: %s' % response)
+			if response['status_code'] != 200:
+				self.cp.log_error("Status code response for HLS variant playlist: %d" % response['status_code'])
+				request.setResponseCode(401)
+				request.finish()
+				return
+
+			def process_url(surl, redirect_url):
+				if not surl.startswith('http'):
+					if surl.startswith('/'):
+						surl = redirect_url[:redirect_url[9:].find('/') + 9] + surl
+					else:
+						surl = redirect_url[:redirect_url.rfind('/') + 1] + surl
+					
+					surl = self.proxify_segment_url(surl)
+
+				return surl
+
+			resp_data = []
+			stream_url_line = False
+
+			for line in iter(response['content'].decode('utf-8').splitlines()):
+				if stream_url_line:
+					resp_data.append(process_url(line, response['url']))
+					stream_url_line = False
+					continue
+
+				if 'URI=' in line:
+					# fix uri to full url
+					uri = line[line.find('URI=') + 4:]
+
+					if uri[0] in ('"', "'"):
+						uri = uri[1:uri[1:].find(uri[0]) + 1]
+
+					line = line.replace(uri, process_url(uri, response['url']))
+
+				if line.startswith("#EXTINF:"):
+					stream_url_line = True
+
+				resp_data.append(line)
+
+			request.setResponseCode(200)
+			request.setHeader('content-type', "application/vnd.apple.mpegurl")
+			request.write(('\n'.join(resp_data) + '\n').encode('utf-8'))
+			request.finish()
+			return
+
+		self.request_http_data_async_simple(url, cbk=p_continue)
+		return self.NOT_DONE_YET
+
+
+	# #################################################################################################
+
+	def get_hls_playlist_data_async(self, url, bandwidth=None, headers=None, cbk=None):
 		'''
 		Processes HLS master playlist from given url and returns new one with only one variant playlist specified by bandwidth (or with best bandwidth if no bandwidth is given)
 		'''
 
-		def process_url(surl):
+		def process_url(surl, redirect_url):
 			if not surl.startswith('http'):
 				if surl.startswith('/'):
-					surl = url[:url[9:].find('/') + 9] + surl
+					surl = redirect_url[:redirect_url[9:].find('/') + 9] + surl
 				else:
-					surl = url[:url.rfind('/') + 1] + surl
+					surl = redirect_url[:redirect_url.rfind('/') + 1] + surl
+				
+				if self.proxy_segments:
+					surl = self.proxify_playlist_url(surl)
 
 			return surl
 
-		try:
-			response = self.hls_session.get(url, headers=headers)
-		except:
-			self.cp.log_exception()
-			return None
+		def p_continue(response, bandwidth):
+#			self.cp.log_error("HLS response received: %s" % response)
 
-		if response.status_code != 200:
-			self.cp.log_error("Status code response for HLS master playlist: %d" % response.status_code)
-			return None
+			if response['status_code'] != 200:
+				self.cp.log_error("Status code response for HLS master playlist: %d" % response['status_code'])
+				return cbk(None)
 
-		if bandwidth == None:
-			bandwidth = 1000000000
-		else:
-			bandwidth = int(bandwidth)
-
-		streams = []
-
-		for m in re.finditer(r'^#EXT-X-STREAM-INF:(?P<info>.+)\n(?P<chunk>.+)', response.text, re.MULTILINE):
-			stream_info = {}
-			for info in re.split(r''',(?=(?:[^'"]|'[^']*'|"[^"]*")*$)''', m.group('info')):
-				key, val = info.split('=', 1)
-				stream_info[key.strip().lower()] = val.strip()
-
-			stream_info['url'] = process_url(m.group('chunk'))
-
-			if int(stream_info.get('bandwidth', 0)) <= bandwidth:
-				streams.append(stream_info)
-
-		streams = sorted(streams, key=lambda i: int(i['bandwidth']), reverse=True)
-		
-		if len(streams) == 0:
-			self.log_error("No streams found for bandwidth %d" % bandwidth)
-			return None
-
-		resp_data = []
-		ignore_next = False
-		stream_to_keep = 'BANDWIDTH=%s' % streams[0].get('bandwidth', 0)
-
-		for line in iter(response.text.splitlines()):
-			if ignore_next:
-				ignore_next = False
-				continue
-
-			if 'URI=' in line:
-				# fix uri to full url
-				uri = line[line.find('URI=') + 4:]
-
-				if uri[0] in ('"', "'"):
-					uri = uri[1:uri[1:].find(uri[0]) + 1]
-
-				line = line.replace(uri, process_url(uri))
-
-			if line.startswith("#EXT-X-STREAM-INF:"):
-				if stream_to_keep in line:
-					resp_data.append(line)
-					resp_data.append(streams[0]['url'])
-
-				ignore_next = True
+			if bandwidth == None:
+				bandwidth = 1000000000
 			else:
-				resp_data.append(line)
+				bandwidth = int(bandwidth)
 
-		return '\n'.join(resp_data) + '\n'
+			streams = []
+
+			response_text = response['content'].decode('utf-8')
+			for m in re.finditer(r'^#EXT-X-STREAM-INF:(?P<info>.+)\n(?P<chunk>.+)', response_text, re.MULTILINE):
+				stream_info = {}
+				for info in re.split(r''',(?=(?:[^'"]|'[^']*'|"[^"]*")*$)''', m.group('info')):
+					key, val = info.split('=', 1)
+					stream_info[key.strip().lower()] = val.strip()
+
+				stream_info['url'] = process_url(m.group('chunk'), response['url'])
+
+				if int(stream_info.get('bandwidth', 0)) <= bandwidth:
+					streams.append(stream_info)
+
+			streams = sorted(streams, key=lambda i: int(i['bandwidth']), reverse=True)
+			
+			if len(streams) == 0:
+				self.cp.log_error("No streams found for bandwidth %d" % bandwidth)
+				return None
+
+			resp_data = []
+			ignore_next = False
+			stream_to_keep = 'BANDWIDTH=%s' % streams[0].get('bandwidth', 0)
+
+			for line in iter(response_text.splitlines()):
+				if ignore_next:
+					ignore_next = False
+					continue
+
+				if 'URI=' in line:
+					# fix uri to full url
+					uri = line[line.find('URI=') + 4:]
+
+					if uri[0] in ('"', "'"):
+						uri = uri[1:uri[1:].find(uri[0]) + 1]
+
+					line = line.replace(uri, process_url(uri, response['url']))
+
+				if line.startswith("#EXT-X-STREAM-INF:"):
+					if stream_to_keep in line:
+						resp_data.append(line)
+						resp_data.append(streams[0]['url'])
+
+					ignore_next = True
+				else:
+					resp_data.append(line)
+
+			cbk('\n'.join(resp_data) + '\n')
+			return
+	
+		self.request_http_data_async_simple(url, cbk=p_continue, headers=headers, bandwidth=bandwidth)
+
 
 	# #################################################################################################
