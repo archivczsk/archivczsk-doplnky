a4b4633a35f8deeeb256aa0eba69b020fced4b26
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2024-02-05 20:56:23 +0100

    rewrite TA3 addon

---
+++ plugin_video_ta3_com/provider.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+from tools_archivczsk.contentprovider.provider import CommonContentProvider
+from tools_archivczsk.contentprovider.exception import AddonErrorException
+import sys
+
+try:
+	from bs4 import BeautifulSoup
+	bs4_available = True
+except:
+	bs4_available = False
+
+class Ta3ContentProvider(CommonContentProvider):
+
+	def __init__(self, settings=None, data_dir=None):
+		CommonContentProvider.__init__(self, 'TA3', settings=settings, data_dir=data_dir)
+		self.ta3 = None
+
+	# ##################################################################################################################
+
+	def root(self):
+		if not bs4_available:
+			self.show_info(self._("In order addon to work you need to install the BeautifulSoup4 using your package manager. Search for package with name:\npython{0}-beautifulsoup4 or python{0}-bs4)").format('3' if sys.version_info[0] == 3 else ''))
+			return
+
+		if self.ta3 == None:
+			from ta3 import TA3
+			self.ta3 = TA3(self)
+
+		self.add_video('TA3 Live', cmd=self.resolve_live_video, video_title="TA3 Live", section='live', download=False)
+		self.add_video('NRSR Live', cmd=self.resolve_live_video, video_title="NRSR Live", section='nrsr', download=False)
+
+		self.add_dir(self._("Archive of programs"), cmd=self.list_archive)
+		self.add_dir(self._("Slovakia"), cmd=self.list_articles, section="slovensko")
+		self.add_dir(self._("Foreign"), cmd=self.list_articles, section="zahranicie")
+		self.add_dir(self._("Economics"), cmd=self.list_articles, section="ekonomika")
+		self.add_dir(self._("Regions"), cmd=self.list_articles, section="regiony")
+		self.add_dir(self._("Sport"), cmd=self.list_articles, section="sport")
+		self.add_dir(self._("Press conferences"), cmd=self.list_articles, section="tlacove-besedy")
+		self.add_dir(self._("Society"), cmd=self.list_articles, section="spolocnost")
+		self.add_dir(self._("Auto-moto"), cmd=self.list_articles, section="auto-moto")
+		self.add_dir(self._("Health"), cmd=self.list_articles, section="zdravie")
+		self.add_dir('новини', cmd=self.list_articles, section="ukrajinski-novyny")
+
+	# ##################################################################################################################
+
+	def safe_call(self, cmd, *args, **kwargs):
+		try:
+			ret = cmd(*args, **kwargs)
+		except:
+			self.log_exception()
+			self.show_error(self._("Error extracting data from ta3.com. Format of the site is probably changed and addon needs to be modified in order to work again. Please report this problem to addon authors."))
+			ret = None
+
+		return ret
+
+	# ##################################################################################################################
+
+	def list_articles(self, section, page=1):
+		articles, max_page = self.safe_call(self.ta3.get_articles, section, page)
+
+		self.log_debug("Adding %s" % str(articles))
+		for p in articles:
+			info_labels = {
+				'plot': p['desc']
+			}
+			self.add_video(p['name'], p['img'], info_labels, cmd=self.resolve_video, url=p['url'])
+
+		if max_page != None and page < max_page:
+			self.add_next(page_info=(page+1, max_page), cmd=self.list_articles, section=section, page=page+1)
+
+	# ##################################################################################################################
+
+	def list_archive(self):
+		for p in self.safe_call(self.ta3.get_programs):
+			info_labels = {
+				'plot': p['desc']
+			}
+			self.add_dir(p['name'], p['img'], info_labels, cmd=self.list_episodes, url=p['url'])
+
+	# ##################################################################################################################
+
+	def list_episodes(self, url, page=1):
+		episodes, max_page = self.safe_call(self.ta3.get_episodes, url, page)
+
+		for e in episodes:
+			info_labels = {
+				'plot': e['desc']
+			}
+			self.add_video(e['name'], e['img'], info_labels, cmd=self.resolve_video, url=e['url'])
+
+		if max_page != None and page < max_page:
+			self.add_next(page_info=(page+1, max_page), cmd=self.list_episodes, url=url, page=page+1)
+
+	# ##################################################################################################################
+
+	def resolve_streams(self, url, video_title):
+		for one in self.get_hls_streams(url, self.ta3.req_session):
+			info_labels = {
+				'bandwidth': one['bandwidth'],
+				'quality': one.get('resolution', 'x???').split('x')[1] + 'p'
+			}
+			self.add_play(video_title, one['url'], info_labels=info_labels)
+
+	# ##################################################################################################################
+
+	def resolve_video(self, url):
+		video_url, title = self.safe_call(self.ta3.get_video_url, url)
+		self.resolve_streams(video_url, title)
+
+	# ##################################################################################################################
+
+	def resolve_live_video(self, section, video_title):
+		video_url = self.safe_call(self.ta3.get_live_video_url, section)
+		self.resolve_streams(video_url, video_title)
+
+	# ##################################################################################################################
---
+++ plugin_video_ta3_com/ta3.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
+
+COMMON_HEADERS = {
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0',
+	'Referer': 'https://www.ta3.com/',
+}
+
+# ##################################################################################################################
+
+class TA3(object):
+	def __init__(self, content_provider):
+		self.cp = content_provider
+		self.req_session = self.cp.get_requests_session() if self.cp != None else requests.Session()
+		self.req_session.headers.update(COMMON_HEADERS)
+		self.base_url = 'https://www.ta3.com'
+
+	# ##################################################################################################################
+
+	def call_api(self, url, params=None):
+		if not url.startswith('http'):
+			url = self.base_url + '/' + url
+
+		response = self.req_session.get(url, params=params)
+
+		if response.status_code == 200:
+			return BeautifulSoup(response.content, "html.parser")
+		else:
+			raise Exception('HTTP response code: %d for page %s' % (response.status_code, url))
+
+	def call_apiXXX(self, url, params=None):
+		with open(url, 'r') as f:
+			return BeautifulSoup(f.read(), "html.parser")
+
+	# ##################################################################################################################
+
+	def get_img_url(self, section, element):
+		if element == None:
+			return None
+		url = element['src']
+		if url and not url.startswith('http'):
+			if url.startswith('/'):
+				url = self.base_url + url
+			else:
+				url = self.base_url + '/' + section + '/' + url
+
+		return url
+
+	# ##################################################################################################################
+
+	def get_articles(self, section, page=1):
+		ret = []
+		max_page = None
+		soup = self.call_api(section, params={'page': page})
+		for a in soup.find_all('article', {"class": lambda x: x in ("article--main--hybrid", "article--side",) }):
+			if a.find('span', {'class': 'article_videolabel'}) != None:
+				headline = a.find('div', {'class': 'article_headline'})
+				name_element = headline.find('h4')
+				if name_element == None:
+					name_element = headline.find('h2')
+				name = name_element.find('a').get_text().strip(),
+				ret.append({
+					'name': name[0],
+					'url': a.find('a')['href'],
+					'img': self.get_img_url(section, a.find('picture').find('img')),
+					'desc': '[' + headline.find('span', {'class': 'article_date'}).get_text().strip() + ']\n' + name[0],
+				})
+
+		p = soup.find('div', { 'class': 'section_pagination' })
+		if p != None:
+			p = p.find('span')
+			if p != None:
+				_, max_page = p.get_text().split('/')
+				max_page = int(max_page.strip())
+
+		return ret, max_page
+
+	# ##################################################################################################################
+
+	def get_programs(self):
+		section = 'archiv'
+		ret = []
+		soup = self.call_api(section)
+		div = soup.find("div", {"class": "tvshows_listing_results tvshows_results"})
+		for a in div.find_all('a', {"class": "tvshows_listing_item"}):
+			ret.append({
+				'name': a.find('h4').get_text().strip(),
+				'url': a['href'],
+				'img': self.get_img_url(section, a.find('img')),
+				'desc': a.find('p').get_text().strip(),
+			})
+
+		return ret
+
+	# ##################################################################################################################
+
+	def get_episodes(self, url, page=1):
+		ret = []
+		max_page = None
+		soup = self.call_api(url, params={'page': page})
+
+		div = soup.find('div', {'class': 'episode_listing_grid'})
+		for a in div.find_all('article', {"class": "article--main"}):
+			headline = a.find('div', {'class': 'article_headline'})
+			name = headline.find('h2').find('a').get_text().strip()
+			ret.append({
+				'name': name,
+				'url': headline.find('h2').find('a')['href'],
+				'img': self.get_img_url(url, a.find('picture').find('img')),
+				'desc': '[' + headline.find('span', {'class': 'article_date'}).get_text().strip() + ']\n' + name
+			})
+
+		p = soup.find('div', { 'class': 'section_pagination' })
+		if p != None:
+			p = p.find('span')
+			if p != None:
+				_, max_page = p.get_text().split('/')
+				max_page = int(max_page.strip())
+
+		return ret, max_page
+
+	# ##################################################################################################################
+
+	def get_video_url(self, article):
+		# get videoID
+		soup = self.call_api(article)
+		s = soup.find('div', { 'class': 'detail_text_item_livebox' }).find('script')
+		video_id = re.search(r'"videoId"[\s]*:[\s]*"(.+?)".*', s.get_text(), re.DOTALL).group(1)
+		video_name = re.search(r'"title"[\s]*:[\s]*"(.+?)".*', s.get_text(), re.DOTALL).group(1)
+
+		# get URL pattern
+		resp = self.req_session.get("http://embed.livebox.cz/ta3_v2/vod-source.js")
+		url_config = json.loads(re.search(r'my\.embedurl[\s]*=[\s]*(\[.+\])', resp.text, re.DOTALL).group(1))
+
+		url_pattern = url_config[0]['src']
+		if url_pattern.startswith('//'):
+			url_pattern = 'http:' + url_pattern
+
+		return url_pattern.format(video_id), video_name
+
+	# ##################################################################################################################
+
+	def get_live_video_url(self, section='live'):
+		resp = self.req_session.get("http://embed.livebox.cz/ta3_v2/%s-source.js" % section)
+		url_config = json.loads(re.search(r'my\.embedurl[\s]*=[\s]*(\[.+\])', resp.text, re.DOTALL).group(1))
+
+		url = url_config[0]['src']
+		if url.startswith('//'):
+			url = 'http:' + url
+
+		return url
+
+	# ##################################################################################################################
