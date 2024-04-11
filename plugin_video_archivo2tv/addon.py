169cb5597f6149011c7b156f01349c4a1d0ef831
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2024-04-11 12:37:37 +0200

    remove old not working addons

--- plugin_video_archivo2tv/addon.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from Plugins.Extensions.archivCZSK.engine import client
-
-def run(addon, session, params):
-	client.showInfo("Tento doplnok využíval službu od O2 ktorá skončila a preto doplnok viac nie je funkčný. Náhradou je nový doplnok O2 TV 2.0.")
-
-def main(addon):
-	return run
--- plugin_video_online_files/addon.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from Plugins.Extensions.archivCZSK.engine import client
-
-def run(addon, session, params):
-	client.showInfo("Vývoj a údržba doplnku skončili a doplnok viac už nie je funkčný. Náhradou je nový doplnok Webshare.cz")
-
-def main(addon):
-	return run
