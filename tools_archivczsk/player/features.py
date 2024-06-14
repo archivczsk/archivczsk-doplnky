b2512c22c8ee19272664a3ac2c87111cc5b3702a
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2024-06-14 09:55:37 +0200

    update available exteplayer version to 171

--- tools_archivczsk/player/features.py
+++ tools_archivczsk/player/features.py
@@ -12,8 +12,8 @@ __addon__ = ArchivCZSK.get_addon('tools.archivczsk')
 def _(id):
 	return __addon__.get_localized_string(id)
 
-EXTEPLAYER3_NAME='exteplayer3_170'
-FFMPEG_NAME='ffmpeg_170'
+EXTEPLAYER3_NAME='exteplayer3_171'
+FFMPEG_NAME='ffmpeg_171'
 
 class PlayerFeatures(object):
 	DATA_LOADED = False
