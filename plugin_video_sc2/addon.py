eb02cb9d37fca75c9b3d6aad6cf654d3c12c7296
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2025-05-04 16:17:18 +0200

    scc: addon removed - service was closed

--- plugin_video_sc2/addon.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from tools_archivczsk.contentprovider.archivczsk_provider import ArchivCZSKContentProvider
-from .provider import SccContentProvider
-
-# #################################################################################################
-
-def main(addon):
-	cp = SccContentProvider(addon.settings, data_dir=addon.get_info('data_path'), bgservice=addon.bgservice)
-	return ArchivCZSKContentProvider(cp, addon)
