7b004abc5072dfa7db754f6e4da3154218299bb6
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2025-05-08 20:39:42 +0200

    tools_cenc: use mp4decrypt as shared library
    - add new mp4modify library, that will replace mp4decrypt binary
    - modify calls to use lib instead of binaries
    - add call to remove cenc info from mp4 init section

--- tools_cenc/mp4decrypt.py
+++ tools_cenc/mp4decrypt.py
@@ -1,58 +1,36 @@
 # -*- coding: utf-8 -*-
 import os
-import subprocess
-from Plugins.Extensions.archivCZSK.engine.tools.stbinfo import stbinfo
+import ctypes
 
-tmp_file_id = 1
-DECRYPT_CMD = os.path.join(os.path.dirname(__file__), 'bin', 'mp4decrypt_%s' % stbinfo.hw_arch)
+mp4modify_lib = None
+
+def get_mp4modify():
+	global mp4modify_lib
+
+	if mp4modify_lib == None:
+		from Plugins.Extensions.archivCZSK.engine.tools.stbinfo import stbinfo
+		mp4modify_lib = ctypes.CDLL( os.path.join(os.path.dirname(__file__), 'lib', 'mp4modify_%s.so' % stbinfo.hw_arch) )
+		mp4modify_lib.mp4_cenc_info_remove.argtypes = (ctypes.c_char_p, ctypes.c_int)
+		mp4modify_lib.mp4_decrypt.argtypes = (ctypes.c_char_p, ctypes.c_int, ctypes.c_char_p, ctypes.c_int, ctypes.POINTER(ctypes.c_char_p))
+
+	return mp4modify_lib
+
 
 def mp4decrypt(keys, init_data, enc_data):
-	# this is very simple and unefective implementation that blocks reactor and that is definitely not what we want ...
-	# needs complete rewrite to async ...
-	global tmp_file_id
+	keys_clist = (ctypes.c_char_p * (len(keys) + 1))()
+	for i, k in enumerate(keys):
+		keys_clist[i] = k.encode('utf-8')
 
-	# create temp file with segment data
-	tmp_file_name_init = os.path.join( '/tmp', '._init_%d.mp4' % tmp_file_id)
-	tmp_file_name_in = os.path.join( '/tmp', '._in_%d.m4s' % tmp_file_id)
-	tmp_file_name_out = os.path.join( '/tmp', '._out_%d.m4s' % tmp_file_id)
-	tmp_file_id += 1
-	if tmp_file_id >= 1000:
-		tmp_file_id = 1
+	keys_clist[ len(keys) ] = None
 
-	with open(tmp_file_name_in, 'wb') as f:
-		f.write(enc_data)
+	# WARNING: this function will modify pythons internal enc_data buffer !!!
+	ret = get_mp4modify().mp4_decrypt(init_data, len(init_data), enc_data, len(enc_data), keys_clist)
+	x = enc_data[:ret]
 
-	cmd = [DECRYPT_CMD]
+	return x
 
-	for k in keys:
-		cmd.append('--key')
-		cmd.append(k)
-
-	if init_data != None:
-		with open(tmp_file_name_init, 'wb') as f:
-			f.write(init_data)
-
-		cmd.append('--fragments-info')
-		cmd.append(tmp_file_name_init)
-
-	cmd.append(tmp_file_name_in)
-	cmd.append(tmp_file_name_out)
-
-	try:
-		subprocess.check_call( cmd )
-	except:
-		data_out = None
-	else:
-		with open(tmp_file_name_out, 'rb') as f:
-			data_out = f.read()
-	finally:
-		if init_data != None:
-			os.remove(tmp_file_name_init)
-
-		os.remove(tmp_file_name_in)
-		try:
-			os.remove(tmp_file_name_out)
-		except:
-			pass
-
-	return data_out
+def mp4_cenc_info_remove(data):
+	# needed to create copy of data, because function will modify it
+	data = data + b'X'
+	ret = get_mp4modify().mp4_cenc_info_remove(data, len(data)-1)
+	return data[:ret]
