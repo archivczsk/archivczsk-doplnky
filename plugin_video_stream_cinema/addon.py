e7ee4292cc32627bc46efddee42a5ef3ab0afb60
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2025-05-15 21:22:22 +0200

    sc: removed addon

--- plugin_video_stream_cinema/addon.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from tools_archivczsk.contentprovider.archivczsk_provider import ArchivCZSKContentProvider
-from .provider import StreamCinemaContentProvider
-
-# #################################################################################################
-
-def main(addon):
-	cp = StreamCinemaContentProvider(addon.settings, data_dir=addon.get_info('data_path'))
-	return ArchivCZSKContentProvider(cp, addon)
