1bbd97e736e78c3bbb469d243a4fc0b13a2e5def
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2025-12-03 13:21:35 +0100

    prehrajto: many fixes because of page change
    - fixed login to premium
    - fixed reading of file size
    - fixed reading results from multiple pages

--- plugin_video_prehrajto/prehrajto.py
+++ plugin_video_prehrajto/prehrajto.py
@@ -4,6 +4,9 @@ import re
 from tools_archivczsk.contentprovider.exception import AddonErrorException
 from tools_archivczsk.string_utils import strip_accents
 from tools_archivczsk.parser.js import get_js_data
+from tools_archivczsk.debug.http import dump_json_request
+from tools_archivczsk.compat import urlparse, parse_qs
+
 
 try:
 	from bs4 import BeautifulSoup
@@ -37,6 +40,8 @@ class PrehrajTo(object):
 		else:
 			response = self.req_session.get(url=endpoint, params=params, allow_redirects=allow_redirects)
 
+#		dump_json_request(response)
+
 		if raw:
 			return response
 
@@ -61,27 +66,60 @@ class PrehrajTo(object):
 		data = {
 			"email": user,
 			"password": password,
-			'_submit': 'Přihlásit+se',
-			'remember': 'on',
-			'do': 'login-loginForm-submit'
+			'_do': 'homepageLoginForm-loginForm-submit',
+			'login': 'Přihlásit+se'
 		}
-		soup = self.call_api('', data=data)
 
-		title = soup.find('div',attrs={'class': 'user-panel'})
-		if title:
-			title = title.find('span',attrs={'class': 'color-positive'})
+		params={
+			'frm': 'homepageLoginForm-loginForm'
+		}
 
-		if title == None:
+		# load cookies
+		self.call_api('', raw=True)
+		response = self.call_api('', data=data, params=params, raw=True)
+
+		try:
+			if parse_qs(urlparse(response.url).query).get('afterLogin',['0'])[0] == '1':
+				# login OK - check premium
+				if self.check_premium():
+					self.cp.log_info("Prehraj.to premium account activated")
+					self.is_premium = True
+				else:
+					self.cp.log_error("Prehraj.to premium account is inactive")
+					self.is_premium = False
+			else:
+				self.cp.log_error("Prehraj.to login failed - wrong username/password or login procedure has been changed")
+				self.is_premium = False
+		except:
+			self.cp.log_error("Login procedure crashed")
+			self.cp.log_exception()
 			self.is_premium = False
-			self.cp.log_error("Prehraj.to premium account is inactive")
-		else:
-			self.cp.log_info("Prehraj.to premium account activated")
-			self.is_premium = True
 
 		self.user = user
 		self.password = password
 
 		return self.is_premium
+
+	# ##################################################################################################################
+
+	def check_premium(self):
+		response = self.call_api('profil', raw=True)
+
+		if urlparse(response.url).path == '/':
+			self.cp.log_error("Failed to load user profile")
+			# wrong login
+			return False
+
+		soup = BeautifulSoup(response.content, 'html.parser')
+		for div in soup.find_all('div', {'class': 'section__content section__content--border'}):
+			p = div.find('p', {'class': 'text-medium margin-bottom-0'})
+			if p != None and 'PREMIUM' in p.get_text():
+				premium_text = div.find('div', {'class': 'cell small-6 text-right'}).find('p', {'class': 'text-medium margin-bottom-0'}).get_text().strip()
+				if premium_text.startswith("Vyprší za"):
+					self.cp.log_info("Premium status: %s" % premium_text)
+					return True
+
+		return False
 
 	# ##################################################################################################################
 
@@ -98,17 +136,17 @@ class PrehrajTo(object):
 		keyword = strip_accents(keyword)
 
 		while True:
-			soup = self.call_api('hledej/' + quote(keyword.encode('utf-8')), params={'vp-page': page})
+			soup = self.call_api('hledej/' + quote(keyword.encode('utf-8')), params={'videoListing-visualPaginator-page': page})
 
 			for item in soup.find_all('div', attrs={'class': 'video__picture--container'}):
 				title_and_link = item.find('a', attrs={'class': 'video video--small video--link'})
-				size = item.find('div', attrs={'class': 'video__tag video__tag--size'})
+				size = item.find('div', attrs={'class': 'video__tag video__tag--size'}) or item.find('div', attrs={'class': 'video__tag video__tag--size video__tag--size-alone'})
 				img = item.find('img', attrs={'data-next': '2'})
 #				time = soup.find('div', attrs={'class': 'video__tag video__tag--size'})
-				if title_and_link and size:
+				if title_and_link:
 					videos.append({
 						'title': title_and_link.get('title').strip(),
-						'size': size.text.strip(),
+						'size': '?' if size == None else size.text.strip(),
 						'img': img.get('src') if img else None,
 						'id': title_and_link.get('href').strip().lstrip('./')
 					})
