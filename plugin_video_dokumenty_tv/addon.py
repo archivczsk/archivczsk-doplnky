8bb2a22475dec2f1fea103e0e1b3e239a553e099
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2026-02-23 12:42:57 +0100

    dokumenty: remove addon - the content of the site is trash now

--- plugin_video_dokumenty_tv/addon.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from tools_archivczsk.contentprovider.archivczsk_provider import ArchivCZSKContentProvider
-from .http_handler import DokumentyTVHTTPRequestHandler
-from .provider import DokumentyTvContentProvider
-
-# #################################################################################################
-
-def main(addon):
-	return ArchivCZSKContentProvider(DokumentyTvContentProvider, addon, http_cls=DokumentyTVHTTPRequestHandler)
