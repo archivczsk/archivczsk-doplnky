a30d06296a711154cdbec8ff77b1f5f6f7318849
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2026-02-26 10:23:14 +0100

    tools_cenc: dynamic playready support

--- tools_cenc/mp4decrypt.py
+++ tools_cenc/mp4decrypt.py
@@ -17,8 +17,8 @@ def get_mp4modify():
 	if mp4modify_lib == None:
 		from Plugins.Extensions.archivCZSK.engine.tools.stbinfo import stbinfo
 		mp4modify_lib = ctypes.CDLL( os.path.join(os.path.dirname(__file__), 'lib', 'mp4modify_%s_%d.so' % (stbinfo.hw_arch, API_LEVEL)) )
-		mp4modify_lib.mp4_cenc_info_remove.argtypes = (ctypes.c_char_p, ctypes.c_int)
-		mp4modify_lib.mp4_decrypt.argtypes = (ctypes.c_char_p, ctypes.c_int, ctypes.c_char_p, ctypes.c_int, ctypes.POINTER(ctypes.c_char_p))
+		mp4modify_lib.mp4_cenc_info_remove.argtypes = (ctypes.c_char_p, ctypes.c_int, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_int))
+		mp4modify_lib.mp4_decrypt.argtypes = (ctypes.c_char_p, ctypes.c_int, ctypes.c_char_p, ctypes.c_int, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_int))
 		mp4modify_lib.mp4_pssh_get.argtypes = (ctypes.c_char_p, ctypes.c_int)
 		mp4modify_lib.mp4_pssh_get.restype = ctypes.POINTER(ctypes.c_char)
 
@@ -28,25 +28,28 @@ def get_mp4modify():
 def mp4decrypt(keys, init_data, enc_data):
 	keys_clist = (ctypes.c_char_p * (len(keys) + 1))()
 	for i, k in enumerate(keys):
-		keys_clist[i] = k.encode('utf-8')
+		keys_clist[i] = k.encode('ascii')
 
 	keys_clist[ len(keys) ] = None
 
-	# WARNING: this function will modify pythons internal enc_data buffer !!!
-	ret = get_mp4modify().mp4_decrypt(init_data, len(init_data), enc_data, len(enc_data), keys_clist)
-	return enc_data[:ret]
+	out_ptr = ctypes.c_char_p()
+	out_size = ctypes.c_int()
+	get_mp4modify().mp4_decrypt(init_data, len(init_data), enc_data, len(enc_data), keys_clist, ctypes.byref(out_ptr), ctypes.byref(out_size))
+	result = ctypes.string_at(out_ptr, out_size.value)
+	libc.free(out_ptr)
+	return result
 
 def mp4_cenc_info_remove(data):
-	# needed to create copy of data, because function will modify it
-	data = data + b'X'
-	ret = get_mp4modify().mp4_cenc_info_remove(data, len(data)-1)
-	return data[:ret]
+	out_ptr = ctypes.c_char_p()
+	out_size = ctypes.c_int()
+	get_mp4modify().mp4_cenc_info_remove(data, len(data), ctypes.byref(out_ptr), ctypes.byref(out_size))
+	result = ctypes.string_at(out_ptr, out_size.value)
+	libc.free(out_ptr)
+	return result
 
 def mp4_pssh_get(data):
-	# only WV DRM is currently supported
-
 	ret = get_mp4modify().mp4_pssh_get(data, len(data))
-	pssh, kid = ctypes.cast(ret, ctypes.c_char_p).value.split(b';')
+	wv_pssh, pr_pssh, kid = ctypes.cast(ret, ctypes.c_char_p).value.split(b';')
 	libc.free(ret)
 
-	return cenc_init(unhexlify(pssh)) if pssh else None, kid.decode('utf-8')[8:].lower(), 'wv'
+	return cenc_init(unhexlify(wv_pssh)) if wv_pssh else None, cenc_init(unhexlify(pr_pssh)) if pr_pssh else None, kid.decode('ascii')[8:].lower()
