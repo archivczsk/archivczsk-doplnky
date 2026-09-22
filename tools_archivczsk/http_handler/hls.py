006d22c01094701eba96bace7ec1c8b4a2a68653
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2026-09-22 13:32:35 +0200

    tools: added multiaudio cleanup directly to HLS master processor

--- tools_archivczsk/http_handler/hls.py
+++ tools_archivczsk/http_handler/hls.py
@@ -17,6 +17,7 @@ class HlsMasterProcessor(HlsPlaylist):
 		super(HlsMasterProcessor, self).__init__(url)
 		self.request_handler = request_handler
 		self.hls_info = hls_info or {}
+		self.enable_multiaudio = (not hasattr(request_handler, 'enable_hls_multiaudio')) or request_handler.enable_hls_multiaudio
 
 	# #################################################################################################
 
@@ -37,6 +38,34 @@ class HlsMasterProcessor(HlsPlaylist):
 				for p in playlist_group:
 					if p.playlist_url:
 						p.playlist_url = self.request_handler.hls_proxify_variant_url(p.playlist_url, self.hls_info)
+
+	# #################################################################################################
+
+	def cleanup_master_playlist(self):
+		super(HlsMasterProcessor, self).cleanup_master_playlist()
+
+		if not self.enable_multiaudio or not self.hls_info.get('enable_multiaudio', True):
+			external_audio_cnt = 0
+			internal_audio_cnt = 0
+
+			for p in self.audio_playlists:
+				if p.playlist_url:
+					external_audio_cnt += 1
+				else:
+					internal_audio_cnt += 1
+
+
+			if external_audio_cnt > 0 and internal_audio_cnt > 0:
+				# exteplayer3 has problems playing HLS when there is a mix of audio tracks with external URI and internal (embedded to video track - without URI in playlist)
+				# only external audio is played and switching to internal one doesn't work
+				# workaround to play embedded audio is to remove all external tracks
+
+				playlists = []
+				for p in self.audio_playlists:
+					if not p.playlist_url:
+						playlists.append(p)
+
+				self.audio_playlists = playlists
 
 
 # #################################################################################################
